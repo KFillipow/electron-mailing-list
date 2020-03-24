8bcd9fe31d3cb68895e1a6c6f4705baef5be5560
Kai Fillipow <kaifillipow@yahoo.com>
2020-03-24 14:48:43 -0700

    test

--- main.js
+++ main.js
@@ -17,9 +17,27 @@ function createWindow () {
 app.whenReady().then(createWindow)
 
 function ParseCustomers(){
-
+  //go through all customers in JSON
+  var TopMargins      = 36;  // 0.5  inches
+  var SideMargins     = 14;  // 0.19 inches
+  var VerticalPitch   = 72;  // 1    inch
+  var HorizontalPitch = 198; // 2.75 inches
+  var LabelHeight     = 72;  // 1    inch
+  var LabelWidth      = 190; // 2.63 inches
+  var LabelsPerPage   = 30;
+  var LabelRows       = 10;
+  var LabelCols       = 3;
+  var LabelCounter    = 0;
+  var strLines = str.split("\n");
+  for (var i in strLines){
+    var obj = JSON.parse(strLines[i]);
+    CreateLabels(LabelCounter);
+    if(LabelCounter => (LabelRows * LabelCols)-1){
+      LabelCounter = 0;
+    }
+  }
 }
 
-function CreateLabels(){
-  
+function CreateLabels(LabelCounter){
+
 }
